f359e2fc13c0a2c552c71683e74415dd748dfced
Konrad Janowski <kejzin@gmail.com>
2020-05-11 12:33:52 +0200

    Use argparse in CmdInterface kit, delete tests for it as they are to complicated

--- CmdInterface.py
+++ CmdInterface.py
@@ -1,20 +1,8 @@
-import sys
-import logging
+import argparse
 
+def parse_argument():
+    parser = argparse.ArgumentParser
+    parser.add_argument("-i", " --input_files_paths", help="path to wave files")
+    args = parser.parse_args()
+    return args
 
-class CmdInterface:
-    @staticmethod
-    def get_path_from_cmd():
-        """
-        get firs argument from cmd. In use case it should be wave files path
-        Returns
-        -------
-            path: str
-                first argument from cmd, should be path to wave files
-        """
-        try:
-            path = sys.argv[1]
-        except IndexError:
-            logging.error('Path not entered. Please enter path to wave files')
-            sys.exit()
-        return path
