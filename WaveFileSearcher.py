9e0704ceecc511604d6f8a244db8292106f047aa
Konrad Janowski <kejzin@gmail.com>
2020-05-11 14:20:23 +0200

    Delete REFERENCE exvlude from wave files in find_wave_files_paths

--- WaveFileSearcher.py
+++ WaveFileSearcher.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import ntpath
-from CmdInterface import CmdInterface
+import CmdInterface as cmd
 
 
 class WaveFileSearcher:
@@ -11,16 +11,17 @@ class WaveFileSearcher:
         Find wave files under path given in cmd. Path must be a directory containing wav files and REFERENCE.wav file.
         Returns
         -------
-           wave_files_and_reference_paths: [str]
+           wave_files_paths: [str]
                 touple of two list. First is wave files paths to analize, second is reference file path.
         """
-        path = CmdInterface.get_path_from_cmd()
+        cmd_args = cmd.parse_argument()
+        path = cmd_args.input_file
         if os.path.isfile(path):
             raise NotADirectoryError('path is a file not a directory')
         elif os.path.isdir(path):
             path_content = os.listdir(path)
             wave_files_paths = ["{}/{}".format(path, file_path) for file_path in path_content if (".wav" in file_path
-                                or ".WAV" in file_path)and("REFERENCE" not in file_path)]
+                                or ".WAV" in file_path)]
         else:
             logging.error('{} is no valid path'.format(path))
             raise FileNotFoundError('{} is not a valid path'.format(path))
@@ -31,5 +32,5 @@ class WaveFileSearcher:
         print('I found {} files. Files names are: {}'.format(len(wave_files_names), wave_files_names))
         print('')
 
-        wave_files_and_reference_paths = wave_files_paths
-        return wave_files_and_reference_paths
+        wave_files_paths = wave_files_paths
+        return wave_files_paths
